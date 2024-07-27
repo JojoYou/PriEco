726d908fdf2d9f9bfa65aa45cf0af7142603c913
JojoYou <jojoyou@jojoyou.org>
2024-07-27 19:50:23 +0200

    image style update, increased css version

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $gTime = microtime(true);
 //Development mode (Get search results from json files in ./Controller/dev folder)
 $dev = false;
 //CSS version
-$cssver = 200;
+$cssver = 203;
 //Variable, controls reloading on settings change
 $reload = false;
 
